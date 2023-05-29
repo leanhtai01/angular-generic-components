d87108861a89e8e28d153e9e2e83d155f36e6074
leanhtai01 <leanhtai01@gmail.com>
2023-05-29 22:20:01 +0700

    pass tree data source as component input

--- src/app/mat-tree-with-checkboxes/mat-tree-with-checkboxes.component.ts
+++ src/app/mat-tree-with-checkboxes/mat-tree-with-checkboxes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {FlatTreeControl} from "@angular/cdk/tree";
 import {MatTreeFlatDataSource, MatTreeFlattener} from "@angular/material/tree";
 import {SelectionModel} from "@angular/cdk/collections";
@@ -21,114 +21,6 @@ export interface LayerManagerInfoDto {
   layer7Managers: LayerManagerInfo[];
 }
 
-const DATA: LayerManagerInfoDto = {
-  layer1Managers: [
-    {
-      managerId: 11581385,
-      managerName: 'Salvino, Mike Joseph',
-      managerEmail: 'mike.salvino@dxc.com',
-      orgId: '65000001',
-      orgName: 'DXC Technology',
-    },
-  ],
-  layer2Managers: [
-    {
-      managerId: 583173,
-      managerName: 'Sharp, Ken P',
-      managerEmail: 'ken.sharp@dxc.com',
-      orgId: '65000026',
-      orgName: 'Corp Fn - Finance',
-    },
-    {
-      managerId: 655831,
-      managerName: 'Deckelman Jr., William Leroy',
-      managerEmail: 'bill.deckelman@dxc.com',
-      orgId: '65000027',
-      orgName: 'Corp Fn - Legal',
-    },
-  ],
-  layer3Managers: [
-    {
-      managerId: 9015882,
-      managerName: 'Copley, Natasha',
-      managerEmail: 'ncopley@dxc.com',
-      orgId: '65017700',
-      orgName: 'Chief of Staff – APAC',
-    },
-    {
-      managerId: 11571318,
-      managerName: 'Owens, Jimmie',
-      managerEmail: 'Jimmie.owens@dxc.com',
-      orgId: '65000186',
-      orgName: 'Global Cyber Security',
-    },
-  ],
-  layer4Managers: [
-    {
-      managerId: 11744547,
-      managerName: 'Gilmore, Lou Anne',
-      managerEmail: 'louanne.gilmore@dxc.com',
-      orgId: '65018128',
-      orgName: 'Global Delivery Assurance',
-    },
-    {
-      managerId: 11590932,
-      managerName: 'Wentink, Robbert',
-      managerEmail: 'robbert.wentink@dxc.com',
-      orgId: '65017460',
-      orgName: 'Nordics',
-    },
-  ],
-  layer5Managers: [
-    {
-      managerId: 20408890,
-      managerName: 'Kochan, Kamilia',
-      managerEmail: 'kamilia.chouchane@dxc.com',
-      orgId: '65018438',
-      orgName: 'BPS Poland GD- Operations',
-    },
-    {
-      managerId: 11735404,
-      managerName: 'Pasternak, Sean B.',
-      managerEmail: 'sean.pasternak@dxc.com',
-      orgId: '65016596',
-      orgName: 'External Communications, PR',
-    },
-  ],
-  layer6Managers: [
-    {
-      managerId: 11721271,
-      managerName: 'Ralph, Glen Dale',
-      managerEmail: 'glen.ralph@dxc.com',
-      orgId: '65015082',
-      orgName: 'Cloud Delivery Advisory',
-    },
-    {
-      managerId: 11755883,
-      managerName: 'Yaman, Mehmet',
-      managerEmail: 'mehmet.yaman@dxc.com',
-      orgId: '65017872',
-      orgName: 'ES CA BC Public Sector - Advanced Solutions',
-    },
-  ],
-  layer7Managers: [
-    {
-      managerId: 75006424,
-      managerName: 'Ølholm, Arnita',
-      managerEmail: 'arnita.oelholm@dxc.com',
-      orgId: '65018034',
-      orgName: 'Regional Payroll',
-    },
-    {
-      managerId: 11598689,
-      managerName: 'Guevarra, Renalett',
-      managerEmail: 'renalett.guevarra@dxc.com',
-      orgId: '65015487',
-      orgName: 'Cloud Infra / ITO PH - AMS/EU - Batch Shared 2',
-    },
-  ],
-};
-
 /**
  * Node for item
  */
@@ -148,61 +40,14 @@ export class ItemFlatNode {
   ) {}
 }
 
-function buildLayerWithManagersTree(data: LayerManagerInfoDto): ItemNode[] {
-  const LAYERS: any = {
-    layer1Managers: 'Layer 1',
-    layer2Managers: 'Layer 2',
-    layer3Managers: 'Layer 3',
-    layer4Managers: 'Layer 4',
-    layer5Managers: 'Layer 5',
-    layer6Managers: 'Layer 6',
-    layer7Managers: 'Layer 7',
-  };
-
-  const listNode: ItemNode[] = [];
-
-  Object.entries(data).forEach(([key, managers]) => {
-    const layer: ItemNode = {
-      item: LAYERS[key],
-      children: [],
-    };
-
-    managers.forEach(
-      (managerInfo: {
-        managerName: any;
-        managerId: any;
-        managerEmail: any;
-        orgId: any;
-        orgName: any;
-      }) => {
-        const manager: ItemNode = {
-          item: managerInfo.managerName,
-          children: [],
-          data: {
-            managerId: managerInfo.managerId,
-            managerName: managerInfo.managerName,
-            managerEmail: managerInfo.managerEmail,
-            orgId: managerInfo.orgId,
-            orgName: managerInfo.orgName,
-          },
-        };
-
-        layer.children.push(manager);
-      }
-    );
-
-    listNode.push(layer);
-  });
-
-  return listNode;
-}
-
 @Component({
   selector: 'efp-mat-tree-with-checkboxes',
   templateUrl: './mat-tree-with-checkboxes.component.html',
   styleUrls: ['./mat-tree-with-checkboxes.component.scss'],
 })
 export class MatTreeWithCheckboxesComponent {
+  @Input() treeDataSource: ItemNode[] = [];
+
   /** Map from flat node to nested node. This helps us find the nested node to be modified */
   flatNodeMap = new Map<ItemFlatNode, ItemNode>();
 
@@ -238,7 +83,7 @@ export class MatTreeWithCheckboxesComponent {
   }
 
   ngOnInit(): void {
-    this.dataSource.data = buildLayerWithManagersTree(DATA);
+    this.dataSource.data = this.treeDataSource;
   }
 
   getLevel = (node: ItemFlatNode) => node.level;
